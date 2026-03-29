dc364d231b94a08173fb8e5ab30dde700b2eabc9
Dakota <dakota.o.dev@gmail.com>
2026-03-28 19:21:56 -0700

    feat: PDF extractor
    Closes [INGEST] Extract clean text from PDFs using PyMuPDF
    Fixes #6

---
+++ src/ingestion/pdf_extractor.py
@@ -0,0 +1,38 @@
+import pymupdf4llm
+import os
+from langchain_text_splitters import MarkdownTextSplitter
+
+RAW_PDFS = "/Users/dakota/repos/parenting-rag/data/raw/pdfs"
+PROCESSED_PDFS = "/Users/dakota/repos/parenting-rag/data/processed"
+
+class PDFExtractor:
+
+
+    def __init__(self, ingestion_dir_path: str, output_dir_path: str, chunk_size=1000, chunk_overlap=200):
+        self.ingestion_dir_path=ingestion_dir_path
+        self.output_dir_path=output_dir_path
+        self.chunk_size=chunk_size
+        self.chunk_overlap=chunk_overlap
+        self.text_splitter = MarkdownTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+
+
+    def extract(self):
+        pdf_files = []
+        for pdf in os.listdir(self.ingestion_dir_path):
+            if pdf.endswith(".pdf"):
+                pdf_files.append(pdf)
+
+        for pdf in pdf_files:
+            pdf_path = os.path.join(self.ingestion_dir_path, pdf)
+            md_text = pymupdf4llm.to_markdown(pdf_path)
+
+            chunks = self.text_splitter.split_text(md_text)
+            processed_pdf_path = os.path.join(self.output_dir_path, pdf.replace(".pdf", ".txt"))
+            with open(processed_pdf_path, "w") as f:
+                f.write(md_text)
+                # for chunk in chunks:
+                    # f.write(chunk  + "\n\n")
+
+if __name__ == "__main__":
+    extractor = PDFExtractor(ingestion_dir_path=RAW_PDFS, output_dir_path=PROCESSED_PDFS)
+    extractor.extract() 
